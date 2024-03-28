e5df36155e6bd417bc12606a1eeaa68a6acc3e01
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-03-28 12:01:19 +0100

    Add files via upload
    Added file with mongoose schema to my App.

---
+++ models.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+let movieSchema= mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: [String],
+    Director: { type: String, required: true },
+    Featured: Boolean 
+});
+
+let userSchema = mongoose.Schema({
+    UserName: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthdate: Date,
+    FavoriteMovies: [{type: String, required: true}],  
+
+});
+
+userSchema.statics.hashPassword =(password) => {
+    return bcrypt.hashSync(password, 10);
+};
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
+let directorSchema = mongoose.Schema({
+    Name: {String},
+    Bio: {String},
+    //Year 
+    Birth: {type: Date},
+    Death:{type: Date}
+})
+
+let Movie =mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+let Director = mongoose.model('Directors', directorSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Director = Director;
