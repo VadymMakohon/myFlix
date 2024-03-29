ea389122b39f2b630672d0c8a285b0dcda8279d1
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-03-29 15:15:52 +0100

    Add files via upload
    Added 2 file Passport and Authentication

---
+++ passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy,
+Models = require('./models.js'),
+passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+JWTStrategy =passportJWT.Strategy,
+ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: 'UserName',
+            passwordField: 'Password',
+        },
+        async (username, password, callback) => {
+            console.log(`${username} ${password}`);
+            await Users.findOne({ UserName: username})
+            .then((user) =>{
+                if (!user) {
+                    console.log('incorect username');
+                    return callback(null, false, {
+                        message: 'Incorrect username or password.',
+                    });
+                }
+                if (!user.validatePassword(password)){
+                    console.log('incorrect password');
+                    return callback(null, false, {message: 'Incorrect password.'});
+                }
+                console.log('finished');
+                return callback(null, user);
+            })
+            .catch((error) => {
+                if (error) {
+                    console.log(error);
+                }
+            })
+        }
+    )
+);
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+    return await Users.findById(jwtPayload._id)
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((error) => {
+        return callback(error)
+    });
+}));
