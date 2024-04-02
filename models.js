e7500b4c52b9716e00977faf86c79e736848e447
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-04-02 23:14:57 +0200

    Update models.js
    Updated som minor points

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-let movieSchema= mongoose.Schema({ 
+let movieSchema = mongoose.Schema({ 
     Title: {type: String, required: true}, 
     Description: {type: String, required: true}, 
     Genre: { Name: {String}, Description: {String}, }, 
@@ -18,7 +19,7 @@ let userSchema = mongoose.Schema({
 
 });
 
-userSchema.statics.hashPassword =(password) => {
+userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 };
 userSchema.methods.validatePassword = function(password) {
@@ -32,7 +33,7 @@ let directorSchema = mongoose.Schema({
     Death:{type: Date}
 })
 
-let Movie =mongoose.model('Movie', movieSchema);
+let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 let Director = mongoose.model('Directors', directorSchema);
 
