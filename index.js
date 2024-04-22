65d49b7bdbfa79d43d46d9f687e832d4f6684970
VadymMakohon <errorks7@gmail.com>
2024-04-22 12:21:39 +0200

    removed block cors lines

--- index.js
+++ index.js
@@ -33,17 +33,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn`t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 //use Auth.js
 let auth = require("./auth")(app);
 
