80b7f48937e69ca1a756620c75bb37426c7388fa
VadymMakohon <errorks7@gmail.com>
2024-05-07 20:58:16 +0200

    changed secretkey

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ const express = require("express"),
   app = express();
 const cors = require('cors');
 
-const allowedOrigins = ['http://localhost:1234']; // Add your frontend origin(s) here
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Check if the request origin is in the allowedOrigins array
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    Models = require('./models.js'),
+    Models = require('../models/models'),
     passportJWT = require('passport-jwt');
 
 let Users = Models.User,
@@ -14,7 +14,7 @@ passport.use(
             passwordField: 'password',
         },
         async (username, password, callback) => {
-            console.log(`${username} ${password}`);
+            console.log(`${username}`);
             await Users.findOne({ username: username })
                 .then((user) => {
                     if (!user) {
@@ -25,30 +25,37 @@ passport.use(
                     }
                     if (!user.validatePassword(password)) {
                         console.log('incorrect password');
-                        return callback(null, false, { message: 'Incorrect password.' });
+                        return callback('Incorrect password.', false, {
+                            message: 'Incorrect password.',
+                        });
                     }
                     console.log('finished');
                     return callback(null, user);
                 })
-                .catch((error) => {
-                    if (error) {
-                        console.log(error);
-                        return callback(error);
+                .catch((err) => {
+                    if (err) {
+                        console.log(err);
+                        return callback(err);
                     }
-                })
+                });
         }
     )
 );
 
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'HRKU-c8ce1acc-2155-4f1f-93b6-3599e628f309'
-}, async (jwtPayload, callback) => {
-    return await Users.findById(jwtPayload._id)
-        .then((user) => {
-            return callback(null, user);
-        })
-        .catch((error) => {
-            return callback(error)
-        });
-}));
+passport.use(
+    new JWTStrategy(
+        {
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_SECRET,
+        },
+        async (jwtPayload, callback) => {
+            return await Users.findById(jwtPayload._id)
+                .then((user) => {
+                    return callback(null, user);
+                })
+                .catch((err) => {
+                    return callback(err);
+                });
+        }
+    )
+);
