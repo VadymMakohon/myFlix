35ff663cef63b110ced329a68fbe2e08f6bb30b7
VadymMakohon <errorks7@gmail.com>
2024-05-08 09:46:59 +0200

    changed models

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    Models = require('./models'),
+    Models = require('./models.js'),
     passportJWT = require('passport-jwt');
 
 let Users = Models.User,
