0b8232f008a7757fdff2458ac1afcc13ce3f0e06
VadymMakohon <errorks7@gmail.com>
2024-05-15 19:56:59 +0200

    changed the secret in pasport.js

--- passport.js
+++ passport.js
@@ -45,7 +45,7 @@ passport.use(
 passport.use(new JWTStrategy(
     {
         jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: 'some-secret-string'
+        secretOrKey: 'your_jwt_secret'
     }, async (jwtPayload, callback) => {
         return await Users.findById(jwtPayload._id)
             .then((user) => {
