3eb08964d912f262ac09c5b15d83ea04db9119f0
VadymMakohon <errorks7@gmail.com>
2024-05-17 09:25:38 +0200

    updated pasport.use

--- passport.js
+++ passport.js
@@ -42,17 +42,15 @@ passport.use(
     )
 );
 
-passport.use(new JWTStrategy(
-    {
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: 'your_jwt_secret'
-    }, async (jwtPayload, callback) => {
-        return await Users.findById(jwtPayload._id)
-            .then((user) => {
-                return callback(null, user);
-            })
-            .catch((err) => {
-                return callback(err);
-            });
-    }
-));
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+    return await Users.findById(jwtPayload._id)
+        .then((user) => {
+            return callback(null, user);
+        })
+        .catch((error) => {
+            return callback(error)
+        });
+}));
