28ab61f5d92719c6b165bbfe8be6ebd7cac0a7dc
VadymMakohon <errorks7@gmail.com>
2024-06-02 21:54:18 +0200

    updated CORS’ allowed origins

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const express = require("express"),
 const cors = require("cors");
 require('dotenv').config();
 
-app.use(cors());
+// Define allowed origins for CORS
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://testsite.com',
+  'http://localhost:1234',
+  'https://movie-app-777777.netlify.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,7 +56,6 @@ mongoose.connect(process.env.CONNECTION_URI, {
   .catch(error => {
     console.error('MongoDB connection error:', error);
   });
-
 
 // Welcome page
 app.get("/", (req, res) => {
